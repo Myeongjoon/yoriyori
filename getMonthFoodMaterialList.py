70e5f6807878972537aa703596707d09a6eb31cc
Myeongjoon <kimmj8409@gmail.com>
2018-08-23 21:03:54 +0900

    랜덤 기능 적용

--- getMonthFoodMaterialList.py
+++ getMonthFoodMaterialList.py
@@ -1,14 +1,12 @@
-#이달의 요리 재료 기능
-def getMonthFoodMaterialList(request):
-    from bs4 import BeautifulSoup
-    import json
-    import urllib.request
-    import urllib.parse
-    request_json = request.get_json()
-    mon = '01'
-    if request.args and 'mon' in request.args:
-        mon = request.args.get('mon')
-    web_url = "http://koreanfood.rda.go.kr/kfi/foodMonth/list?menuId=PS03599&mon"+mon
+from bs4 import BeautifulSoup
+from getRandomIDataInArray import getRandomIDataInArray
+import json
+import urllib.request
+import urllib.parse
+
+#이달의 요리 재료 기능 - 코어
+def getMonthFoodMaterialListCore(mon):
+    web_url = "http://koreanfood.rda.go.kr/kfi/foodMonth/list?menuId=PS03599&mon="+mon
     with urllib.request.urlopen(web_url) as response:
         html = response.read()
         soup = BeautifulSoup(html, 'html.parser')
@@ -16,8 +14,20 @@ def getMonthFoodMaterialList(request):
         for i in range(3):
             if i == 0:
                 children = all_divs[0].findChildren("dd" , recursive=True)
-                resText = ""
+                words = []
                 for child in children:
-                    resText = resText + child.contents[0] + " "
-    return resText
-    
+                    words.append(child.contents[0])
+    return words
+#이달의 요리 재료 기능 - 구글 클라우드 용
+def getMonthFoodMaterialList(request):
+    request_json = request.get_json()
+    mon = '01'
+    if request.args and 'mon' in request.args:
+        mon = request.args.get('mon')
+    return getRandomIDataInArray(getMonthFoodMaterialListCore(mon),3)
+
+
+#이달의 요리 재료 기능 - 테스트
+if __name__ == "__main__":
+    print(getRandomIDataInArray(getMonthFoodMaterialListCore("01"),3))
+    print(getRandomIDataInArray(getMonthFoodMaterialListCore("02"),3))
---
+++ getRandomIDataInArray.py
@@ -0,0 +1,14 @@
+import random
+
+#리스트에 있는 랜덤 값 str으로
+def getRandomIDataInArray(arr,num):
+    res = ""
+    for i in range(num):
+        idx = random.randrange(0,num)
+        res = res + arr.pop(idx) + " "
+    return res
+
+#이달의 요리 재료 기능 - 테스트
+if __name__ == "__main__":
+    print(getRandomIDataInArray(["123","456","1123"],2))
+
