e044d819bb537dbaba9c6dc2e5c1a9e81d17603f
Myeongjoon <kimmj8409@gmail.com>
2018-08-27 22:54:07 +0900

    문장 잘 돌아가도록 변경

--- yoriJJangRouter.py
+++ yoriJJangRouter.py
@@ -4,6 +4,16 @@ import json
 import urllib.request
 import urllib.parse
 
+#이달의 식재료 리스트 리턴
+def getMonthFoodMaterialMessage(intent):
+    return getMonthFoodMaterialList(intent) + "입니다"
+
+#이달의 식재료 리스트 리턴
+def getMonthFoodMaterialList(intent):
+    slots = intent['slots']
+    mon = slots['targetMonth']['value']
+    return getRandomIDataInArray(getMonthFoodMaterialListCore(mon),3)
+
 #이달의 요리 재료 기능 - 코어
 def getMonthFoodMaterialListCore(mon):
     web_url = "http://koreanfood.rda.go.kr/kfi/foodMonth/list?menuId=PS03599&mon="+mon
@@ -18,21 +28,23 @@ def getMonthFoodMaterialListCore(mon):
                 for child in children:
                     words.append(child.contents[0])
     return words
-#이달의 요리 재료 기능 - 구글 클라우드 용
+#구글 클라우드 라우터
 def yoriJJangRouter(request):
-    request_json = request.get_json()
-    mon = '01'
-    if request.args and 'request' in request.args:
-        req = request.args.get('request')
-        slots = req.slots
-        targetMonth = slots.targetMonth
-        return targetMonth
-    return getRandomIDataInArray(getMonthFoodMaterialListCore(mon),3)
+    request_json = request
+    req = request_json['request']
+    intent = req['intent']
+    if intent['name'] == '이달의식재료' :
+        return getMonthFoodMaterialMessage(intent)
 
 
 #이달의 요리 재료 기능 - 테스트
 if __name__ == "__main__":
+    import json
+
     print(getRandomIDataInArray(getMonthFoodMaterialListCore("01"),3))
     print(getRandomIDataInArray(getMonthFoodMaterialListCore("02"),3))
-}
-}
+    from pprint import pprint
+
+    with open('test.json') as f:
+        data = json.load(f)
+    print(yoriJJangRouter(data))
