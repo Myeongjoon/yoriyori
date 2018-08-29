6c55d906fe799441913573e8857c58f21371224e
Myeongjoon <kimmj8409@gmail.com>
2018-08-30 00:01:22 +0900

    from pprint import pprint

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 from getRandomIDataInArray import getRandomIDataInArray
 from getMonthFoodMaterialMessage import getMonthFoodMaterialListCore
 from main import yoriJJangCore
+from pprint import pprint
 import json
 
 #이달의 요리 재료 기능 - 테스트
 if __name__ == "__main__":
     print(getRandomIDataInArray(getMonthFoodMaterialListCore("01"),3))
     print(getRandomIDataInArray(getMonthFoodMaterialListCore("02"),3))
-    from pprint import pprint
+    
     with open('test.json') as f:
         data = json.load(f)
     print(yoriJJangCore(data))
