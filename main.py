43d5f514a2cfdbac2a07d8d748c28f81e52ef833
Joon <kimmj8409@gmail.com>
2018-09-09 17:21:47 +0900

    remove image test

--- main.py
+++ main.py
@@ -6,46 +6,6 @@ import Biz.RecipeBiz
 import urllib.request
 import urllib.parse
 
-
-#이미지 테스트
-def returnImageView():
-    res = {
-      "type": "ImageText",
-      "imageUrl": {
-        "type": "url",
-        "value": ""
-      },
-      "mainText": {
-        "type": "string",
-        "value": "리오넬 메시"
-      },
-      "referenceText": {
-        "type": "string",
-        "value": "검색결과"
-      },
-      "referenceUrl": {
-        "type": "url",
-        "value": "https://m.search.contentproviderdomain.com/search?where=m&sm=mob_lic&query=%eb%a6%ac%ec%98%a4%eb%84%ac+%eb%a9%94%ec%8b%9c+%ec%86%8c%ec%86%8d%ed%8c%80"
-      },
-      "subTextList": [
-        {
-          "type": "string",
-          "value": "FC 바르셀로나"
-        }
-      ],
-      "thumbImageType": {
-        "type": "string",
-        "value": "인물"
-      },
-      "thumbImageUrl": {
-        "type": "url",
-        "value": "http://sstatic.contentproviderdomain.net/people/3/201607071816066361.jpg"
-      }
-    }
-    return res
-
-
-
 #요리짱 응답 생성
 def CreateResponse(request,value):
     values = {"value" : value,"lang" : "ko","type" : "PlainText"}
@@ -75,8 +35,6 @@ def yoriJJangCore(request):
         return RecipeBiz.getFoodRecipe(intent)
     elif name == '이달의식재료' :
         return MessageBiz.getMonthFoodMaterialMessage(intent)
-    elif name == '이미지' :
-        return returnImageView()
     elif name =='Clova.ExitExtensionIntent' :
         return MessageBiz.getExitExtensionIntent()
     else :
